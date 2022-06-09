4d9bc23697e820940e28cd484d9c77eeda9ad919
Danilo Pianini <danilo.pianini@unibo.it>
2022-06-09 22:32:36 +0200

    build: enable git hooks generation for conventional commits

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.gradle.enterprise") version "3.10.2"
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.0.12"
 }
 
 gradleEnterprise {
@@ -8,6 +9,11 @@ gradleEnterprise {
         termsOfServiceAgree = "yes"
         publishOnFailure()
     }
+}
+
+gitHooks {
+    commitMsg { conventionalCommits() }
+    createHooks()
 }
 
 rootProject.name = "Template-for-Gradle-Plugins"
