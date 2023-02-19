2780dfc19e37f3ce0e90479b4bdc312dfbf90d09
Danilo Pianini <danilo.pianini@gmail.com>
2023-02-19 20:12:03 +0100

    build: drop unused deprecated compiler flag

--- build.gradle.kts
+++ build.gradle.kts
@@ -68,7 +68,7 @@ kotlin {
         compilations.all {
             kotlinOptions {
                 allWarningsAsErrors = true
-                freeCompilerArgs = listOf("-XXLanguage:+InlineClasses", "-opt-in=kotlin.RequiresOptIn")
+                freeCompilerArgs = listOf("-opt-in=kotlin.RequiresOptIn")
             }
         }
     }
