48662c2b1f0dc82a5dfb888d49f5f04db29cb33e
Danilo Pianini <danilo.pianini@unibo.it>
2024-07-09 12:29:38 +0200

    ci(release): witch to ECMA6 configuration for semantic release

--- release.config.mjs
+++ release.config.mjs
@@ -1,9 +1,9 @@
-var publishCmd = `
+const publishCmd = `
 ./gradlew uploadKotlinOSSRHToMavenCentralNexus uploadPluginMavenToMavenCentralNexus uploadPluginMarkerMavenToMavenCentralNexus release || exit 1
 ./gradlew publishPlugins -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET || exit 2
 ./gradlew publishKotlinOSSRHPublicationToGithubRepository publishPluginMavenPublicationToGithubRepository || true
-`
-var config = require('semantic-release-preconfigured-conventional-commits');
+`;
+import config from 'semantic-release-preconfigured-conventional-commits' assert { type: "json" };
 config.plugins.push(
     [
         "@semantic-release/exec",
@@ -14,4 +14,4 @@ config.plugins.push(
     "@semantic-release/github",
     "@semantic-release/git",
 )
-module.exports = config
+export default config;
