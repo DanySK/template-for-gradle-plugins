f90fe5c007ea7099e297ec8cb928bbdd8ae21d7e
Danilo Pianini <danilo.pianini@unibo.it>
2025-06-29 19:06:29 +0200

    fix(release): switch to the Maven Central Portal

--- release.config.mjs
+++ release.config.mjs
@@ -1,7 +1,7 @@
 const publishCmd = `
-./gradlew uploadAll releaseStagingRepositoryOnMavenCentral || exit 1
+./gradlew publishAllPublicationsToProjectLocalRepository zipMavenCentralPortalPublication releaseMavenCentralPortalPublication || exit 1
 ./gradlew publishPlugins -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET || exit 2
-./gradlew publishKotlinOSSRHPublicationToGithubRepository publishPluginMavenPublicationToGithubRepository || true
+./gradlew publishOSSRHPublicationToGithubRepository publishPluginMavenPublicationToGithubRepository publishPluginMarkerMavenPublicationToGithubRepository || true
 `;
 import config from 'semantic-release-preconfigured-conventional-commits' with { type: "json" };
 config.plugins.push(
