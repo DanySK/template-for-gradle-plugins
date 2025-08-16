7e6b7d93b88706cf2b7aad0d00e49a2ea3178b3b
Danilo Pianini <danilo.pianini@gmail.com>
2025-08-16 13:42:15 +0200

    test: add `-Dorg.gradle.kotlin.dsl.scriptCompilationAvoidance=false` to work around gradle/gradle#34115

--- src/test/kotlin/org/danilopianini/gradle/test/Tests.kt
+++ src/test/kotlin/org/danilopianini/gradle/test/Tests.kt
@@ -20,63 +20,48 @@ import org.gradle.testkit.runner.TaskOutcome
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-class Tests :
-    StringSpec(
-        {
-            val testkitProperties = javaClass.classLoader.getResource("testkit-gradle.properties")?.readText()
-            checkNotNull(testkitProperties) {
-                "No file testkit-gradle.properties was generated"
-            }
-            val scan = ClassGraph().enableAllInfo()
-                .acceptPackages(Tests::class.java.`package`.name)
-                .scan()
-            scan.getResourcesWithLeafName("test.yaml").flatMap { resource ->
-                log.debug("Found test list in {}", resource)
-                val yamlFile = File(resource.classpathElementFile.absolutePath + "/" + resource.path)
-                val testConfiguration =
-                    Config {
-                        addSpec(Root)
-                    }.from.yaml.inputStream(resource.open())
-                testConfiguration[Root.tests].map { it to yamlFile.parentFile }
-            }.forEach { (test, location) ->
-                log.debug("Test to be executed: {} from {}", test, location)
-                val testFolder = folder { location.copyRecursively(toFile()) }
-                log.debug("Test has been copied into {} and is ready to get executed", testFolder)
-                test.description {
-                    testFolder.resolve("gradle.properties").writeText(testkitProperties)
-                    val result =
-                        GradleRunner
-                            .create()
-                            .withProjectDir(testFolder.toFile())
-                            .withPluginClasspath()
-                            .withArguments(test.configuration.tasks + test.configuration.options)
-                            .run { if (test.expectation.failure.isEmpty()) build() else buildAndFail() }
-                    println(result.tasks)
-                    println(result.output)
-                    test.expectation.output_contains.forEach {
-                        result.output shouldContain it
-                    }
-                    test.expectation.output_doesnt_contain.forEach {
-                        result.output shouldNotContain it
-                    }
-                    test.expectation.success.forEach {
-                        result.outcomeOf(it) shouldBe TaskOutcome.SUCCESS
-                    }
-                    test.expectation.failure.forEach {
-                        result.outcomeOf(it) shouldBe TaskOutcome.FAILED
-                    }
-                    test.expectation.file_exists.forEach {
-                        val file =
-                            File("${testFolder.absolutePathString()}/${it.name}").apply {
-                                shouldExist()
-                                shouldBeAFile()
-                            }
-                        it.validate(file)
-                    }
+class Tests : StringSpec({
+    val testkitProperties = javaClass.classLoader.getResource("testkit-gradle.properties")?.readText()
+    checkNotNull(testkitProperties) {
+        "No file testkit-gradle.properties was generated"
+    }
+    val scan = ClassGraph().enableAllInfo().acceptPackages(Tests::class.java.`package`.name).scan()
+    scan.getResourcesWithLeafName("test.yaml").flatMap { resource ->
+        log.debug("Found test list in {}", resource)
+        val yamlFile = File(resource.classpathElementFile.absolutePath + "/" + resource.path)
+        val testConfiguration = Config { addSpec(Root) }.from.yaml.inputStream(resource.open())
+        testConfiguration[Root.tests].map { it to yamlFile.parentFile }
+    }.forEach { (test, location) ->
+        log.debug("Test to be executed: {} from {}", test, location)
+        val testFolder = folder { location.copyRecursively(toFile()) }
+        log.debug("Test has been copied into {} and is ready to get executed", testFolder)
+        test.description {
+            testFolder.resolve("gradle.properties").writeText(testkitProperties)
+            val result = GradleRunner.create()
+                .withProjectDir(testFolder.toFile())
+                .withPluginClasspath()
+                .withArguments(
+                    test.configuration.tasks +
+                        test.configuration.options +
+                        "-Dorg.gradle.kotlin.dsl.scriptCompilationAvoidance=false",
+                )
+                .run { if (test.expectation.failure.isEmpty()) build() else buildAndFail() }
+            println(result.tasks)
+            println(result.output)
+            test.expectation.output_contains.forEach { result.output shouldContain it }
+            test.expectation.output_doesnt_contain.forEach { result.output shouldNotContain it }
+            test.expectation.success.forEach { result.outcomeOf(it) shouldBe TaskOutcome.SUCCESS }
+            test.expectation.failure.forEach { result.outcomeOf(it) shouldBe TaskOutcome.FAILED }
+            test.expectation.file_exists.forEach {
+                val file = File("${testFolder.absolutePathString()}/${it.name}").apply {
+                    shouldExist()
+                    shouldBeAFile()
                 }
+                it.validate(file)
             }
-        },
-    ) {
+        }
+    }
+}) {
     companion object {
         val log: Logger = LoggerFactory.getLogger(Tests::class.java)
 
